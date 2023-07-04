95ca42189d6353455e06d92c10d991bdbd88759b
balti <baltin.d.c.s@gmail.com>
2023-07-04 22:26:10 +0300

    eeeee

---
+++ 8 function.py
@@ -0,0 +1,17 @@
+def greet_user(username):
+    print(f"Hello,{username.title()}")
+greet_user('jesse')
+greet_user('111jesse')
+
+print(f" ")
+def favorite_book(name,author):
+    print(f"Favorite book is, {name.title()}")
+    print(f"Author book is, {author.title()}")
+    full_name=f"{name} {author}"
+    return full_name.title()
+bookname=favorite_book('book1','name1')
+favorite_book('11 minutes', 'Koelio')
+favorite_book(name='11 11111minutes', author='Koelio')
+print(bookname)
+
+
