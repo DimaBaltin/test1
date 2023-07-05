c34d673eded1b05a3122823b1f9c5ac7c20ee792
balti <baltin.d.c.s@gmail.com>
2023-07-05 11:55:34 +0300

    While

--- 8 function.py
+++ 8 function.py
@@ -14,4 +14,17 @@ favorite_book('11 minutes', 'Koelio')
 favorite_book(name='11 11111minutes', author='Koelio')
 print(bookname)
 
-
+def get_name (f_name,l_name):
+    full_name=f"{f_name} {l_name}"
+    return full_name.title()
+while True:
+    print("\nPlease tell me your name:")
+    print("enter 'q' at any time to quit")
+    fir_name=input("First name:")
+    if fir_name =='q':
+        break
+    sec_name=input("Second name:")
+    if sec_name =='q':
+        break
+    formatted_name=get_name(fir_name,sec_name)
+    print(f"\nHello,{formatted_name}")
