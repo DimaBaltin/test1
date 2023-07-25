d2bf5d11e5712908311683912242a1279acde93e
balti <baltin.d.c.s@gmail.com>
2023-07-26 00:32:50 +0300

    Car

--- Class/CarClass.py
+++ Class/CarClass.py
@@ -13,6 +13,8 @@ class Car:
         print(f"This car has {self.odometer_reading} miles on it.")
 
 my_new_car = Car('audi', 'a4', 2019)
+
+
 print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
