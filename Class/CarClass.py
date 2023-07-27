2be2028ac5fe55065b240754850aadb759816aab
balti <baltin.d.c.s@gmail.com>
2023-07-27 22:33:33 +0300

    Car

--- Class/CarClass.py
+++ Class/CarClass.py
@@ -12,9 +12,30 @@ class Car:
     def read_odometer(self):
         print(f"This car has {self.odometer_reading} miles on it.")
 
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("you cab't roll back an odometer!")
+
+    def increment_odometer(self,miles):
+        self.odometer_reading += miles
+
+
 my_new_car = Car('audi', 'a4', 2019)
-
-
 print(my_new_car.get_descriptive_name())
+
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+my_new_car.update_odometer(24)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(22100)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
